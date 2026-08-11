28ad803865ddb008652ef0b4c7aa2501eb69add5
1beluga <hei.toky.2@gmail.com>
2026-08-11 23:20:40 +0300

    feat(student): add StudentRepository with full CRUD SQL queries

---
+++ src/repository/StudentRepository.ts
@@ -0,0 +1,89 @@
+import { pool } from "../config/db";
+import { CreateStudentDTO } from "../model/students";
+
+export class StudentRepository {
+
+  async findAll() {
+    const result = await pool.query("SELECT * FROM students");
+    return result.rows;
+  }
+
+  async findById(id: number) {
+    const result = await pool.query("SELECT * FROM students WHERE id = $1", [id]);
+    return result.rows[0];
+  }
+
+  async findByEmail(email: string) {
+    const result = await pool.query("SELECT * FROM students WHERE email = $1" ,[email]);
+    return result.rows[0];
+  }
+
+  async updatePut(id: number, data: CreateStudentDTO) {
+    const query = `
+        UPDATE students
+        SET first_name = $1, last_name = $2, email = $3
+        WHERE id = $4
+        RETURNING *
+      `;
+
+    const values = [data.firstName, data.lastName, data.email, id];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  }
+
+  async updatePatch(id: number, data: Partial<CreateStudentDTO>) {
+    const fields: string[] = [];
+    const values: any[] = [];
+    let index = 1;
+
+    if (data.firstName !== undefined) {
+      fields.push(`first_name = $${index}`);
+      values.push(data.firstName);
+      index++;
+    }
+
+    if (data.lastName !== undefined) {
+      fields.push(`last_name = $${index}`);
+      values.push(data.lastName);
+      index++;
+    }
+
+    if (data.email !== undefined) {
+      fields.push(`email = $${index}`);
+      values.push(data.email);
+      index++;
+    }
+
+    if (fields.length == 0) {
+      return this.findById(id);
+    }
+
+    values.push(id);
+    const query = `
+        UPDATE students
+        SET ${fields.join(", ")}
+        WHERE id = $${index}
+        RETURNING *
+      `;
+
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  }
+
+  async create(data: CreateStudentDTO) {
+    const query = `
+        INSERT INTO students (first_name, last_name, email)
+        VALUES ($1, $2, $3)
+        RETURNING *
+      `;
+
+    const values = [data.firstName, data.lastName, data.email];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  }
+
+  async delete(id: number) {
+    const result = await pool.query("DELETE FROM students WHERE id = $1", [id]);
+    return result.rowCount;
+  }
+}
